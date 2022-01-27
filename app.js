5f46ad72716d4ba1306d754f5dd37f665027e85a
tomdanbi <bohyun611.kim@gmail.com>
2022-01-27 12:46:57 +0900

    app.js work 1

---
+++ app.js
@@ -0,0 +1,17 @@
+function generate() {
+
+  const issuser = "bota.com";
+  const username = "botamin611@naver.com";
+  const securityKey = "BBCKBCMBCHBO";
+
+  if(username == null) {
+    console.log("회원이없습니다.");
+
+  } 
+
+  const users = {
+    name : 'tom',
+    age : 8,
+    sex : 'man'
+  }
+}
